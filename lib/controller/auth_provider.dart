0c2772270502c684b158c3603b1b6f7b8b9e3895
dennyraymondd <dennyraymondd@gmail.com>
2021-12-20 02:01:31 +0700

    show profile from firestore

--- lib/controller/auth_provider.dart
+++ lib/controller/auth_provider.dart
@@ -1,13 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthProvider extends ChangeNotifier {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
   final googleSignIn = GoogleSignIn();
 
   GoogleSignInAccount? _user;
+  User? _userSigned;
+
+  User get userSigned => _userSigned!;
 
   GoogleSignInAccount get user => _user!;
+
+  // Future getCurrentUserData() async {
+  //   final currentUser = auth.currentUser;
+
+  //   final userData = await firestore.collection("users").where('uid', isEqualTo: currentUser!.uid).get();
+  // }
+
+  Future<void> addUser(User user) {
+    // Call the user's CollectionReference to add a new user
+    return users.doc(user.uid).set({
+      'full_name': user.displayName, // John Doe
+      'email': user.email, // Stokes and Sons
+      'photoUrl': user.photoURL, // 42
+      'uid': user.uid,
+    }).then((value) async {
+      print("User Added");
+      _userSigned = user;
+    }).catchError((error) => print("Failed to add user: $error"));
+  }
+
+  Future<bool> userExists(String uid) async {
+    return (await firestore.collection("users").where('uid', isEqualTo: uid).get()).docs.length > 1;
+  }
+
+  Future<DocumentSnapshot> getUserDoc() async {
+    final user = auth.currentUser;
+    final ref = firestore.collection('users').doc(user!.uid).get();
+    return ref;
+  }
 
   Future googleLogin() async {
     try {
@@ -21,7 +58,13 @@ class AuthProvider extends ChangeNotifier {
       final credential =
           GoogleAuthProvider.credential(accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
 
-      await FirebaseAuth.instance.signInWithCredential(credential);
+      final UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      final checkUserExist = await userExists(userCredential.user!.uid);
+
+      if (!checkUserExist) {
+        addUser(userCredential.user!);
+      }
 
       notifyListeners();
     } catch (e) {
--- lib/ui/pages/profile_page/profile_page.dart
+++ lib/ui/pages/profile_page/profile_page.dart
@@ -1,12 +1,59 @@
+import 'package:bukalapak_test/controller/auth_provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('profile page'),
-    );
+    final authProvider = Provider.of<AuthProvider>(context);
+    return FutureBuilder<DocumentSnapshot>(
+        future: authProvider.getUserDoc(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Text("Document does not exist");
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+            return SizedBox(
+              width: double.maxFinite,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: CachedNetworkImageProvider(
+                          data['photoUrl'],
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                    "${data['full_name']}",
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  const SizedBox(height: 12),
+                  Text("${data['email']}"),
+                ],
+              ),
+            );
+          }
+
+          return const Text("loading");
+        });
   }
 }
